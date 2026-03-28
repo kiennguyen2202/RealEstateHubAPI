635514ab3d1e6f771e1f074e23cb2fef3c230a0d
Rin <mrkienquangngai2@gmail.com>
2026-03-28 15:15:54 +0700

    Remove offending @ant-design/v5-patch-for-react-19 to fix Vercel Rollup failed to resolve error

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./pages/Home.css";
 import 'antd/dist/reset.css';
-import '@ant-design/v5-patch-for-react-19';
+
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
